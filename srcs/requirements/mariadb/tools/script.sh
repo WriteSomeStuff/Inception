b4feb4c679746b760978ab8bebb76c400e7eeec5
Cheyenne Schabracq <cschabra@f1r3s11.codam.nl>
2025-01-03 13:45:53 +0100

    Remove unused password

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,7 +6,7 @@ service mariadb start
 sleep 5
 
 echo "Running MariaDB commands..."
-mysql -u root --password="$DB_ROOT_PASSWORD" <<EOF
+mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
